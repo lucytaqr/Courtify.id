5af3521085a31e6554df711ed4a3d57d594d1060
Lucyta Qutsyaning Rosydah <lucytaqr08@gmail.com>
2024-01-18 16:42:40 +0700

    fix alert login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -43,14 +43,14 @@
                             <div class="form loginBox">
 
                             @if(session()->has('success'))
-                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <div class="alert alert-success alert-dismissible show" role="alert">
                                 {{ session('success') }}
                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                               </div>
                             @endif
 
                             @if(session()->has('loginError'))
-                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <div class="alert alert-danger alert-dismissible show" role="alert">
                                 {{ session('loginError') }}
                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                               </div>
